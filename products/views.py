fb04b272b6311a9ee1d47b8cd5bc4d9c0c27aec0
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2022-08-30 08:53:54 +0000

    Added search box functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
@@ -6,9 +8,21 @@ def all_products(request):
     """A view to render the list of products page, including sorting & searching"""
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'query' in request.GET:
+            query = request.GET['query']
+            if not query:
+                messages.error(request, "You did not enter a search term!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
-        'products': products
+        'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
