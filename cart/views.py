c2be8c363385612cab1816f8b54e25c9140fd415
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2022-09-01 13:52:26 +0000

    Added shopping bag app, urls and template

---
+++ cart/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+def view_cart(request):
+    """A view to render the cart contents page"""
+    return render(request, 'cart/cart.html')
--- luna_pottery/settings.py
+++ luna_pottery/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'welcome',
     'products',
+    'cart',
 ]
 
 MIDDLEWARE = [
