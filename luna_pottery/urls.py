b38331f56b92685ac8e1ee77684c73db2dadb1b7
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2022-09-24 07:51:01 +0000

    Added 404 page and route

--- luna_pottery/urls.py
+++ luna_pottery/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,3 +14,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'luna_pottery.views.handler404'
