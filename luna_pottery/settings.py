757d27d23d54198a33bc919646ab818c6791f53a
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2022-09-26 12:03:10 +0000

    Attempt to fix email signup bug

--- luna_pottery/settings.py
+++ luna_pottery/settings.py
@@ -208,7 +208,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'luna@example.com'
+    DEFAULT_FROM_EMAIL = 'luna@luna.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
